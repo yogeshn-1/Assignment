54010e06845d4562ac76afdcb2d096b09cdee0bb
yogeshn-1 <nirmalkary@gmail.com>
2024-01-16 13:08:11 +0530

    Updating layout and config file

--- src/Components/Body.tsx
+++ src/Components/Body.tsx
@@ -3,24 +3,30 @@ import styled from "styled-components";
 const Container = styled.div`
   padding: 5px 20px 20px;
   background-color: #f6f6f6;
-  border-radius: 10px;
-  width: 30%;
+  border-radius: 15px;
   box-shadow: 5px 5px 7px 3px #182a46;
 `;
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   gap: 15px;
-  font-size: 24px;
+  width: 30vw;
+  margin-left: auto;
+  margin-right: auto;
 `;
 const Button = styled.button`
-  background: linear-gradient(#4b94c5, #3f79a2);
+  background: linear-gradient(#4b94c5 50%, #3f79a2 50%);
   width: fit-content;
   align-self: flex-end;
   border-radius: 5px;
-  padding: 7px;
+  padding: 10px;
   border: 0;
   color: white;
+  font-size: 20px;
+  cursor: pointer;
+  &:hover {
+    background: linear-gradient(#4b95d6 50%, #3f7ab3 50%);
+  }
 `;
 const Body = () => {
   return (
@@ -31,7 +37,7 @@ const Body = () => {
           <p style={{ fontSize: "12px" }}>
             Please enter your administrator login and password.{" "}
           </p>
-          <input placeholder="Username"></input>
+          <input placeholder="Username" defaultValue="guest"></input>
           <input placeholder="Password"></input>
           <Button>Sign in</Button>
         </Form>
